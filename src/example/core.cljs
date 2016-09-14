8eb2b138028620bb00253cb4ec8b99a6f9d480a2
iovxw <iovxw@outlook.com>
2016-09-14 16:08:41 +0800

    OrbitControls

--- src/example/core.cljs
+++ src/example/core.cljs
@@ -13,24 +13,44 @@
   (.replaceChild js/document.body new-canvas canvas))
 
 (def scene (js/physijs.Scene. "physijs-worker.min.js"))
-(def camera (js/THREE.PerspectiveCamera. 50 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 1 100))
-(set! (.-z (.-position camera)) 10)
 
-(def sphere-geometry (js/THREE.SphereGeometry. 1 12 12))
+(def camera (js/THREE.PerspectiveCamera. 50 (/ (.-innerWidth js/window) (.-innerHeight js/window)) 1 100))
+(.set (.-position camera) 0 5 10)
+
+(defn render []
+  (.render renderer scene camera))
+
+(def controls (js/THREE.OrbitControls. camera))
+(.addEventListener controls "change" render)
+
+(.addEventListener js/window "resize"
+                   #(let [width (.-innerWidth js/window)
+                          height (.-innerHeight js/window)]
+                      (.setSize renderer width height)
+                      (set! (.-aspect camera) (/ width height))
+                      (.updateProjectionMatrix camera)
+                      (.handleResize controls)
+                      (render)))
+
 (def normal-material (js/THREE.MeshNormalMaterial.))
 
-(def sphere (js/physijs.Sphere. sphere-geometry normal-material (clj->js {:mass 0 :type "RIGID"})))
+(def sphere (js/physijs.Sphere. (js/THREE.SphereGeometry. 1 12 12) normal-material (clj->js {:mass 0 :type "RIGID"})))
+(.set (.-position sphere) 0 1 0)
 (.add scene sphere)
 
 (def box (js/physijs.Box. (js/THREE.BoxGeometry. 1 1 1) normal-material (clj->js {:mass 2})))
-(.set (.-position box) 0.13 3 0)
+(.set (.-position box) 0.13 4 0)
 (set! (-> box .-physics .-linear_velocity .-y) -5)
 (.add scene box)
+
+(def plane (js/physijs.Box. (js/THREE.BoxGeometry. 10 0.1 10) normal-material (clj->js {:mass 0 :type "RIGID"})))
+(.add scene plane)
 
 (def physics-framerate (/ 1000 60))
 
 (defn one-step []
-  (.render renderer scene camera)
+  (.update controls)
+  (render)
   (js/setTimeout (.bind (.-step scene) scene (/ physics-framerate 1000) nil one-step)
                  physics-framerate))
 
